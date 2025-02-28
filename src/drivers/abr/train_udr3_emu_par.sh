19a0ae69ffa4a019b3fb89b6fdec26f0850f69ac
Jane Chen <janechen@cs.utexas.edu>
2025-02-28 14:30:44 -0500

    Add emulation parameters for simulation

--- src/drivers/abr/train_udr3_emu_par.sh
+++ src/drivers/abr/train_udr3_emu_par.sh
@@ -5,7 +5,7 @@ save_dir=/mydata/results/abr
 video_size_file_dir=data/abr/video_sizes
 val_trace_dir=data/abr/val_FCC
 total_epoch=75000
-train_name=udr3
+train_name=udr3_emu_par
 config_file=config/abr/${train_name}.json
 
 for seed in 10 20 30; do
