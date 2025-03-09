dc847916788b6488432c73d4d8ebfed0f54e99e6
Jane Chen <janechen@cs.utexas.edu>
2025-03-09 15:36:48 -0400

    fix plot formart, improve test shell

--- src/emulator/abr/pensieve/tests/gen_plot.py
+++ src/emulator/abr/pensieve/tests/gen_plot.py
@@ -6,6 +6,9 @@ def main():
                          ("/users/janechen/Genet/fig_reproduce/sigcomm_artifact/synthetic/udr3_result", True)] #(result_path, if_simulation_result)
     x = ['Emulator', 'Simulator']
     y = []
+
+    plt.figure(figsize=(8, 6))
+
     for result_path, is_simulation in model_directories:
         print(result_path)
         mean, std, total_num = reward_mean_std(result_path, is_simulation)
@@ -14,11 +17,13 @@ def main():
     
     plt.bar(x, y)    
     #plt.figure(figsize=(10, 6))
-    plt.xlabel("Model Name")
-    plt.ylabel("Mean Reward")
-    plt.title("Mean Rewards for Each Model")
-    plt.xticks(rotation=90)
-    plt.tight_layout()
+    plt.xlabel("Model Name", fontsize=20)
+    plt.ylabel("Mean Reward", fontsize=20)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(loc='lower right', fontsize=18, frameon=False)
+
+    plt.tight_layout(pad=1.0)
     plt.savefig("mean_rewards_plot.png")
 
 if __name__ == "__main__":
